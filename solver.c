8157c591a74c7b39eb414e61cc965cba484f4b33
Mathis SERRIER <mserrier@e2r12p18.clusters.42paris.fr>
2022-07-17 10:46:34 +0200

    ay

---
+++ solver.c
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mserrier <mserrier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 09:08:20 by mserrier          #+#    #+#             */
+/*   Updated: 2022/07/17 09:36:37 by mserrier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	verif_caisse(int size, int depl, int sens, int sol[size][size], int cote, int place)
+{
+	int	valeur;
+
+	if (cote % 2 == 0)
+		valeur = sol[depl][place];
+	else
+		valeur = sol[cote][depl];
+	depl += sens;
+	while (depl != -1 && depl != size)
+	{
+		if (sol[depl][place] > valeur)
+			return (0);
+		depl += sens;
+	}
+	while (depl != -1 && depl != size)
+	{
+		if (sol[cote][depl] > valeur)
+			return (0);
+		depl += sens;
+	}
+	return (1);
+}
+
+int	verif_ligne(int size, int cote, int place, int sol[size][size])
+{
+	int	depl;
+	int	sens;
+	int	valeur;
+	int	compt;
+	int	nb;
+
+	compt = 0;
+	if (cote > size / 2)
+		depl = size - 1;
+	else
+		depl = 0;
+	sens = -2 * (depl % (size - 2)) + 1;
+	while (depl != -1 && depl != size)
+	{
+		if (verif_caisse(size, depl, sens, sol, cote, place))
+			compt++;
+		depl += sens;
+	}
+	if (compt == nb)
+		return (1);
+	return (0);
+}
+
+int	soluc(int size, int sol[size][size], int dep[size][size])
+{
+	int	place;
+	int	cote;
+	int	depl;
+
+	cote = 0;
+	while (cote < size)
+	{
+		place = 0;
+		while (place < size)
+		{
+			if (!verif_ligne(size, cote, place, dep[cote][size]))
+				return (0);
+		place++;
+		}
+		cote++;
+	}
+	return (1);
+}
+int verif_ligcol(int size, int *sol[size], int i, int j)
+{
+  int a;
+  int b;
+
+  a = 0;
+  b = 0;
+  while (a < i)
+    {
+      if (sol[a][j] == sol[i][j])
+        return (0);
+      a++;
+    }
+  while (b < j)
+    {
+      if (sol[i][b] == sol[i][j])
+        return (0);
+      b++;
+    }
+  return (1);
+}
+
+int  **crea_tab(int size)
+{
+  int  i;
+  int  j;
+  int **sol;
+
+  i = 0;
+  sol = (int **) malloc (size *  sizeof(int[size]));
+  while (i < size)
+      sol[i] = (int *) malloc(size * sizeof(int));
+  return (sol);
+}
+
+int  **crea_soluc(int size, int tab[size][size], int i, int j)
+{
+  int a;
+
+  a = 0;
+  while (i < size)
+    {
+      j = 0;
+      while (j < size)
+        
+    }
+  return (sol);
+}
+
+int main(void) 
+{
+  
+  return 0;
+}
